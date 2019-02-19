f7210143ec138d4d918fa3beda3264cbb3ec5e4f
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-02-18 21:59:15 -0500

    html mostly done, can get response from query but having trouble targeting how to show in results from JSON

--- assets/js/java.js
+++ assets/js/java.js
@@ -9,10 +9,8 @@ $(document).ready(function() {
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-          // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
-          // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
           console.log("Response:" + response);
-
+            console.log("Response.Docs: " + response.docs);
           var results = response.docs;
           console.log("results: " + results);
           for (var i = 0; i < results.length; i++) {
